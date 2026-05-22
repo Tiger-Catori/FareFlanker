f13533c31857e3233682ada4a1d897ac67133c8e
Tiger-Catori <michaeladeleye365@gmail.com>
2026-05-22 13:43:33 +0100

    searchFlights method controller

---
+++ app-backend/src/main/java/com/flightcomparison/appbackend/controller/AirportController.java
@@ -0,0 +1,4 @@
+package com.flightcomparison.appbackend.controller;
+
+public class AirportController {
+}
---
+++ app-backend/src/main/java/com/flightcomparison/appbackend/controller/FlightSearchController.java
@@ -0,0 +1,40 @@
+package com.flightcomparison.appbackend.controller;
+
+import com.flightcomparison.appbackend.model.dto.FlightResultDTO;
+import com.flightcomparison.appbackend.model.dto.FlightSearchRequest;
+import com.flightcomparison.appbackend.model.dto.RoundTripFlightResultDTO;
+import com.flightcomparison.appbackend.model.enums.TripType;
+import com.flightcomparison.appbackend.service.CurrencyConversionService;
+import com.flightcomparison.appbackend.service.FlightSearchService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/flights")
+public class FlightSearchController {
+    private final FlightSearchService flightSearchService;
+    private final CurrencyConversionService currencyConversionService;
+
+    public FlightSearchController(FlightSearchService flightSearchService, CurrencyConversionService currencyConversionService) {
+        this.flightSearchService = flightSearchService;
+        this.currencyConversionService = currencyConversionService;
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<?> searchFlights(@Valid @RequestBody FlightSearchRequest request) {
+        if (request.getTripType() == TripType.ONE_WAY) {
+            List<FlightResultDTO> results = flightSearchService.searchOneWay(request);
+            return ResponseEntity.ok(results);
+        } else {
+            List<RoundTripFlightResultDTO> results = flightSearchService.searchRoundTrip(request);
+            return ResponseEntity.ok(results);
+        }
+    }
+
+}
