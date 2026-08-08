e67da70531afbb020b35be0d2267fe06cc66fd63
Tiger-Catori <michaeladeleye365@gmail.com>
2026-08-08 10:29:27 +0100

    Adding template file

---
+++ app-frontend/src/pages/Template.jsx
@@ -0,0 +1,12 @@
+import FooterComponent from "../shared/components/Footer"
+import NavbarComponent from "../shared/components/Navbar"
+
+
+const Template = () => {
+  return (
+    <>
+      <NavbarComponent />
+      <FooterComponent />
+    </>
+  )
+}
